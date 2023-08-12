9eb4b1e87dd7298daca8690a7c6abfafafe9a76d
Altheo Saquilayan <altheo.saquilayan@exist.com>
2023-08-12 14:00:08 +0800

    part01: Additional code change

--- courseinfo/src/components/Total.jsx
+++ courseinfo/src/components/Total.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
 const Total = (props) => {
-    const totalExercises = props.parts.reduce((total, part) => total + part.exercises, 0);
+    // const totalExercises = props.parts.reduce((total, part) => total + part.exercises, 0);
+
+    const utility = {
+      doTotal: (parts) =>{
+        return parts.reduce((total, part) => total + part.exercises, 0);
+      }
+    }
 
     return (
       <p>
-        Number of exercise = {totalExercises}
+        Number of exercise = {utility.doTotal(props.parts)}
       </p>
     )
   }
